72321c3550c7b63584c008c9b135aed78a593297
Kvak <kartonanton97@gmail.com>
2025-07-20 02:01:33 +0300

    Fix german warning

--- netlib/netlib/pcap/pcap.h
+++ netlib/netlib/pcap/pcap.h
@@ -1,4 +1,3 @@
-// ReSharper disable CppInconsistentNaming
 #pragma once
 
 namespace pcap
@@ -158,10 +157,10 @@ namespace pcap
         LINKTYPE_STANAG_5066_D_PDU = 237,
         /// <summary>Linux netlink NETLINK NFLOG socket log messages</summary>
         LINKTYPE_NFLOG = 239,
-        /// <summary>Pseudo-header for Hilscher Gesellschaft für Systemautomation mbH netANALYZER devices,
+        /// <summary>Pseudo-header for Hilscher Gesellschaft fr Systemautomation mbH netANALYZER devices,
         /// followed by an Ethernet frame, beginning with the MAC header and ending with the FCS</summary>
         LINKTYPE_NETANALYZER = 240,
-        /// <summary>Pseudo-header for Hilscher Gesellschaft für Systemautomation mbH netANALYZER devices,
+        /// <summary>Pseudo-header for Hilscher Gesellschaft fr Systemautomation mbH netANALYZER devices,
         /// followed by an Ethernet frame, beginning with the preamble, SFD, and MAC header, and ending
         /// with the FCS</summary>
         LINKTYPE_NETANALYZER_TRANSPARENT = 241,
